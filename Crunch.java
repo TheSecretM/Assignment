785a7504ce27e1e42c16aabc3bb95b66b7ae6aa3
mayed <mayedrashed@hotmail.com>
2025-02-12 15:01:50 -0800

    Used static final int variable for modulus to clean up code

--- Crunch.java
+++ Crunch.java
@@ -10,14 +10,16 @@ public class Crunch extends Sounds
     // The name of the file containing the crunching sound.
     private static final String CRUNCH_SOUND_FILE = "crunch.mp3";
     // Used to count the number of times the sound was asked to play.
-    private static int COUNT;
+    private static int COUNT = 0;
+    // The number of times the method should be called to execute.
+    private static final int PLAY_STEP = 15;
 
     /**
      * Constructor for objects of class Crunch
      */
     public Crunch()
     {
-        COUNT = 0;
+        
     }
     
     /**
@@ -27,7 +29,7 @@ public class Crunch extends Sounds
      */
     public void playSound() {
         COUNT++;
-        if(COUNT % 10 == 0) {
+        if(COUNT % PLAY_STEP == 0) {
             playSoundEffects(CRUNCH_SOUND_FILE);
         }
     }
