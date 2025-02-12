f08ddc54f96d211445da8a15c78a7b7053635bb7
mayed <mayedrashed@hotmail.com>
2025-02-12 15:12:54 -0800

    static final int variable for modulus of playing sound instead of number for cleaner code

--- Pooping.java
+++ Pooping.java
@@ -3,21 +3,23 @@
  * Simply a sound of someone pooping...
  *
  * @author Majed Alali and Vinushan Nagentherarajah
- * @version 3.0
+ * @version 3.1
  */
 public class Pooping extends Sounds
 {
     // The name of the file containing the pooping sound.
     private static final String POOP_SOUND_FILE = "turd.mp3";
     // Used to count the number of times the sound was asked to play.
-    private static int COUNT;
+    private static int COUNT = 0;
+    // The number of times the method should be called to execute.
+    private static final int PLAY_STEP = 50;
 
     /**
      * Constructor for objects of class Pooping
      */
     public Pooping()
     {
-        COUNT = 0;
+        
     }
     
     /**
@@ -28,7 +30,7 @@ public class Pooping extends Sounds
     public void playSound() 
     {
         COUNT++;
-        if(COUNT % 10 == 0) {
+        if(COUNT % PLAY_STEP == 0) {
             playSoundEffects(POOP_SOUND_FILE);
         }
     }
